6f8acbee555d19e5e83060ff0270fb7f18ff46ac
Stephen Warren <swarren@wwwdotorg.org>
2022-10-08 14:33:47 -0600

    linuxcnc: pull script: CNC PC IP changed

--- wood-48x48/software/linuxcnc-config/pull-linuxcnc.sh
+++ wood-48x48/software/linuxcnc-config/pull-linuxcnc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd $(dirname "$0")
-exec rsync -avzr --delete fcch@10.1.10.68:/home/fcch/linuxcnc/ ./linuxcnc/
+exec rsync -avzr --delete fcch@10.1.10.26:/home/fcch/linuxcnc/ ./linuxcnc/
