148a1857ef8f55d3cbbfa10a518fc860ba289eae
FCCH CNC PC <info@fortcollinscreatorhub.org>
2023-05-15 20:53:48 -0600

    wood-48x48: fix CNC IP

--- wood-48x48/software/linuxcnc-config/pull-linuxcnc.sh
+++ wood-48x48/software/linuxcnc-config/pull-linuxcnc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd $(dirname "$0")
-exec rsync -avzr --delete fcch@10.1.10.26:/home/fcch/linuxcnc/ ./linuxcnc/
+exec rsync -avzr --delete fcch@10.1.10.110:/home/fcch/linuxcnc/ ./linuxcnc/
