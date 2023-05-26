03ee4b3ac616ba341a71e63488a8c52d536c2c66
Stephen Warren <swarren@wwwdotorg.org>
2023-05-25 20:30:05 -0600

    linuxcnc: add dust collector MQTT request

---
+++ wood-48x48/software/linuxcnc-config/linuxcnc/configs/mesa_linuxcnc/fcch-dust-collector.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+
+import hal
+import paho.mqtt.publish as publish
+import time
+
+mqtt_hostname = '10.1.10.156'
+mqtt_port = 1883
+mqtt_user = None
+mqtt_pass = None
+mqtt_client_id = 'FIXME'
+mqtt_topic = 'stat/cnc-wood-48x48/STATE'
+
+mqtt_auth = None
+if mqtt_user is not None:
+    mqtt_auth = {'username': mqtt_user, 'password': mqtt_pass}
+
+h = hal.component("fcch-dust-collector")
+h.newpin("on", hal.HAL_BIT, hal.HAL_IN)
+h.ready()
+
+val_last_publish = None
+time_last_publish = time.time()
+try:
+    while True:
+        val_now = h.on
+        time_since_last_publish = time.time() - time_last_publish
+        if val_now != val_last_publish or time_since_last_publish > 10:
+            if val_now:
+                msg = 'ON'
+            else:
+                msg = 'OFF'
+            publish.single(
+                mqtt_topic,
+                msg,
+                qos = 0,
+                retain = False,
+                hostname = mqtt_hostname,
+                port = mqtt_port,
+                client_id = mqtt_client_id,
+                keepalive = 60,
+                will = None,
+                auth = mqtt_auth,
+                tls = None)
+            time_last_publish = time.time()
+        time.sleep(1)
+except KeyboardInterrupt:
+    raise SystemExit
